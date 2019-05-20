e783c5de60f2f24f3c0573e82eb9262ad4c44254
Doozy <slobodan_kletnikov@hotmail.com>
2019-05-20 20:45:13 +0200

    Add logging to circuit-braker events

--- circuit-braker/app.py
+++ circuit-braker/app.py
@@ -5,6 +5,26 @@ from flask_restful import Resource, Api
 import redis
 
 
+class DBListener(pybreaker.CircuitBreakerListener):
+    "Listener used by circuit breakers that execute database operations."
+
+    def before_call(self, cb, func, *args, **kwargs):
+        print("Called before the circuit breaker `cb` calls `func`.")
+        pass
+
+    def state_change(self, cb, old_state, new_state):
+        print("Called when the circuit breaker `cb` state changes.")
+        pass
+
+    def failure(self, cb, exc):
+        print("Called when a function invocation raises a system error.")
+        pass
+
+    def success(self, cb):
+        print("Called when a function invocation succeeds.")
+        pass
+
+
 class Test(Resource):
     def get(self):
         try:
@@ -23,14 +43,15 @@ redis = redis.Redis(host='redis')
 
 app = Flask(__name__)
 api = Api(app)
+api.add_resource(Test, '/')
+api.add_resource(Status, '/status')
+
 db_breaker = pybreaker.CircuitBreaker(
     fail_max=3,
     reset_timeout=20,
-    state_storage=pybreaker.CircuitRedisStorage(pybreaker.STATE_CLOSED, redis)
+    state_storage=pybreaker.CircuitRedisStorage(pybreaker.STATE_CLOSED, redis),
+    listeners=[DBListener()]
     )
-
-api.add_resource(Test, '/')
-api.add_resource(Status, '/status')
 
 
 @db_breaker
